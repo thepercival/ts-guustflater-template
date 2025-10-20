561a3c690087922e09019c595498b9d70cb44e6c
thepercival <cdkdev@pm.me>
2025-10-20 11:06:30 +0200

    fix conf

--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,17 @@ module.exports = (config) => {
   config.set({
     basePath: '../..',
     frameworks: ['jasmine'],
-    //...
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox'
+        ]
+      }
+    },
   });
 }
+
+
 
 process.env.CHROME_BIN = require('puppeteer').executablePath();
