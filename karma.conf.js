56c2b7cdd5aad6d6d81ddad3e8fdec6f8f1f9748
thepercival <cdkdev@pm.me>
2025-10-20 11:55:09 +0200

    fix no-sandbox-ai: Update dependencies and Karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,84 @@
+// Karma configuration to use Puppeteer's Chromium when available
+// and to run ChromeHeadless with --no-sandbox flags (useful in CI)
+
+const path = require('path');
+
+let puppeteerExec = null;
+try {
+  // prefer puppeteer if installed in the project
+  const puppeteer = require('puppeteer');
+  if (puppeteer && typeof puppeteer.executablePath === 'function') {
+    puppeteerExec = puppeteer.executablePath();
+    // also expose for tools that honor this env var
+    process.env.PUPPETEER_EXECUTABLE_PATH = puppeteerExec;
+    process.env.CHROME_BIN = puppeteerExec;
+  }
+} catch (e) {
+  // puppeteer not installed; fall back to system CHROME_BIN if set by user/CI
+}
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false
+    },
+    coverageReporter: {
+      dir: path.join(__dirname, './coverage'),
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--remote-debugging-port=0'
+        ]
+      }
+    },
+    // increase timeouts to reduce flaky disconnects
+    captureTimeout: 120000,
+    browserDisconnectTimeout: 20000,
+    browserDisconnectTolerance: 1,
+    browserNoActivityTimeout: 120000,
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
+
+
 // karma.conf.ts
 module.exports = (config) => {
   config.set({
     basePath: '../..',
-    frameworks: ['jasmine'],    
-    ChromeHeadless: {
-      base: 'ChromeHeadless',
-      flags: [
-        '--no-sandbox'
-      ]
-    }
+    frameworks: ['jasmine'],
+      ChromeHeadless: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox'],
+      }
   });
 }
 
-
-
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+const puppeteer = require('puppeteer');
+// console.log(puppeteer);
+process.env.CHROME_BIN = puppeteer.executablePath();
